0719da00969cd57ef178665aabbe075a35d83f4d
Nerio Villalobos <nervill@gmail.com>
2020-10-25 23:06:19 -0300

    modificacion del codigo

--- webautomation.py
+++ webautomation.py
@@ -1,20 +1,43 @@
+import sys
+import re
 from selenium import webdriver
 
-driver = webdriver.Chrome("--log-level=3")
-driver.get('https://login.salesforce.com')
+regex = '^[a-z0-9]'
 
-searchbox = driver.find_element_by_xpath('//*[@id="username"]')
-searchbox.send_keys('nvillalobos@labsxd.com.labsxd')
+if len(sys.argv) >= 4:
+    arg1 = str(sys.argv[1]).strip()
+    arg2 = str(sys.argv[2]).strip()
+    arg3 = str(sys.argv[3]).strip()
+    if arg1 == '' or arg2 == '' or arg3 == '':
+        print ('No posee Argunmento')
+    else:
+        if arg1 == 'login' or arg1 == 'test':
+            print ('Ingresando a Org Tipo:',arg1)
+            if(re.search(regex,arg2)):
+                                 
+                print("Usuario posee sintaxis correcta")
+                driver = webdriver.Chrome()
+                driver.get('https://'+arg1+'.salesforce.com')
 
-searchbox = driver.find_element_by_xpath('//*[@id="password"]')
-searchbox.send_keys('S4l3sf0rc3')
+                searchbox = driver.find_element_by_xpath('//*[@id="username"]')
+                searchbox.send_keys(arg2)
 
-searchButton = driver.find_element_by_xpath('//*[@id="Login"]')
-searchButton.click()
+                searchbox = driver.find_element_by_xpath('//*[@id="password"]')
+                searchbox.send_keys(arg3)
 
-searchtitle = driver.title
+                searchButton = driver.find_element_by_xpath('//*[@id="Login"]')
+                searchButton.click()
 
-if searchtitle == 'Iniciar sesión | Salesforce':
-    driver.get_screenshot_as_file("result_fail_login.png")
+                searchtitle = driver.title
+                if searchtitle == 'Iniciar sesión | Salesforce':
+                    driver.get_screenshot_as_file("result_fail_login.png")
+                    print ('Error de Autenticacion, el usuario o la clave no son correctas')
+                else:
+                    print('Login Satisfactorio en la Org')
+            else:
+                print('El usuario ingresado no posee una sintaxis correcta') 
+        else:
+            print ('Usted coloco un parametro no aceptado, debe colocar login o test')
 else:
-    print('Login Satisfactorio')
+    print ('Usted debe ingresar los tres (3) parametros requeridos para su ejecucion')
+    print ('Ayuda: comando.py <Tipo de Org:[login|test] <Usuario:[usuario de la Org]> <Clave de la Org:[Clave]')
